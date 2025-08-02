6a149cd088c5e5b8443f28591cdb7df7902a5937
CJ191 <ambalivishnu12@gmail.com>
2025-08-02 12:01:20 +0530

    largest of three number

---
+++ largest of 3 no.py
@@ -0,0 +1,10 @@
+x=int(input('Enter First Number '))
+y=int(input('Enter Second Number '))
+z=int(input('Enter Third Number '))
+
+if (x>y):
+    print(x,'is Largest')
+elif(y>z):
+    print(y,'is Largest')
+else:
+    print(z,'is Largest')
