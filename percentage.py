43382303ef7aca723322e16b814647f8bdea48a2
CJ191 <ambalivishnu12@gmail.com>
2025-08-05 02:37:42 +0530

    garding

---
+++ percentage.py
@@ -0,0 +1,15 @@
+x=int(input('Enter The Mark '))
+
+if (x<60):
+    print('Student Failed')
+elif (x>=60 and x<=69):
+    print('Student Scored D+ Grade')
+elif (x>=70 and x<=79):
+    print('Student Scored C+ Grade')
+elif (x>=80 and x<=89):
+    print('Student Scored B+ Grade')
+elif (x>=90 and x<=100):
+    print('Student Scored A+ Grade')
+
+
+
