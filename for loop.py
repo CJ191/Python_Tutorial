e2aa6d74dd216a1ecb17a544d9ee7f28255b1505
CJ191 <ambalivishnu12@gmail.com>
2025-08-08 12:49:02 +0530

    for lopp basics

---
+++ for loop.py
@@ -0,0 +1,32 @@
+# for i in range(5):
+#     print(i)
+
+# for i in range(1,5): #(starting,limit)
+#     print(i)
+
+# for i in range(0,5,2): #(starting,limit,step(increment))
+#     print(i)
+
+# for i in range(5,0,-1):
+#     print(i)
+
+# s='hello world'
+
+# for i in s: 
+#     print(i)
+
+# for i in s:
+#     if i==' ':
+#         break
+#         # pass
+#         # continue
+#     print(i)
+# else:
+#     print('finished')
+
+# inner loop
+
+for i in range(5):
+    print('-------')
+    for j in range(i):
+        print('iteration','i=',i,'j=',j)
