35ee561f79ebdc5a06f3ffe5de81028b876216bf
CJ191 <ambalivishnu12@gmail.com>
2025-08-13 13:08:51 +0530

    added 3 more string questions

--- string questions.py
+++ string questions.py
@@ -10,17 +10,58 @@
 
 # 2 question
 
-s1=input('enter the string\t')
+# s1=input('enter the string\t')
 
-length=len(s1)
+# length=len(s1)
 
-if length>2:
-    new_string=s1[0:2]+s1[-2:]
-    print(new_string)
-elif length==2:
-    new_string=s1[0:2]+s1[0:2]
-    print(new_string)
-elif length<2:
-    print(s1)
+# if length>2:
+#     new_string=s1[0:2]+s1[-2:]
+#     print(new_string)
+# elif length==2:
+#     new_string=s1[0:2]+s1[0:2]
+#     print(new_string)
+# elif length<2:
+#     print(s1)
 
  
+# 3 question
+
+# x = input('Enter the String')
+# upper=0
+# lower=0
+# digit=0
+# special=0
+# for i in x:
+#     if i.isupper():
+#         upper+=1
+#     elif i.islower():
+#         lower+=1
+#     elif i.isdigit():
+#         digit+=1
+#     else:
+#         special+=1
+# print('there are',upper,'Upper Case')
+# print('there are',lower,'Lower Case')
+# print('there are',digit,'Digits')
+# print('there are',special,'Special Characters')
+
+# 4 question
+
+# str=input('Enter your String\t')
+
+# length=len(str)
+
+# if str.endswith('ing'):
+#     print(str+'ly')
+# elif length>=2:
+#     print(str+'ing')
+# elif length<2:
+#     print(str)
+
+# 5 question
+
+string=input('enter your string\t')
+sub=input('enter your sub-string\t')
+
+index=string.index(sub)
+print(index)
