4366c48f9d4d61c8b4078cbb17d8eb4ff2afc505
CJ191 <ambalivishnu12@gmail.com>
2025-08-21 12:45:44 +0530

    unpacking

--- dicts.py
+++ dicts.py
@@ -1,60 +1,86 @@
 
-#  Dictionary
-# it is mutable data type
-# also know as maping data type
-# it define as key : value pairs
-# py dict aanu jsile object
+# #  Dictionary
+# # it is mutable data type
+# # also know as maping data type
+# # it define as key : value pairs
+# # py dict aanu jsile object
 
-dict1={'name' : 'vishnu','place' : 'Calicut'}
-print(dict1)
-print(type(dict1))
+# dict1={'name' : 'vishnu','place' : 'Calicut'}
+# print(dict1)
+# print(type(dict1))
 
-dict2={
-    'name' : 'vishnu',
-    'age' : 25,
-    'place' : 'Calicut'
-}
-print(dict2)
+# dict2={
+#     'name' : 'vishnu',
+#     'age' : 25,
+#     'place' : 'Calicut'
+# }
+# print(dict2)
 
-# defining using constructer
+# # defining using constructer
 
-dict3=dict(name= 'vishnu',place= 'kozhikode')
-print(dict3)
+# dict3=dict(name= 'vishnu',place= 'kozhikode')
+# print(dict3)
 
-# dict4=dict(
-#     name= input('Enter your Name\t'),
-#     age=int(input('Enter your Age\t')),
-#     place=input('Enter your Place\t')
-# )
-# print(dict4)
-print(dict2['age'])
-print(dict2.keys())
-print(dict2.values())
-print(dict2.items())
+# # dict4=dict(
+# #     name= input('Enter your Name\t'),
+# #     age=int(input('Enter your Age\t')),
+# #     place=input('Enter your Place\t')
+# # )
+# # print(dict4)
+# print(dict2['age'])
+# print(dict2.keys())
+# print(dict2.values())
+# print(dict2.items())
 
-# x=input('Enter key\t')
+# # x=input('Enter key\t')
 
-# if x in dict2:
-#     print('yes')
-# else:
-#     print('no')
+# # if x in dict2:
+# #     print('yes')
+# # else:
+# #     print('no')
 
-# single value change cheyan
-dict2={
-    'name' : 'vishnu',
-    'age' : 25,
-    'place' : 'Calicut'
-}
-print(dict2)
-dict2['age']=20
-print(dict2)
+# # single value change cheyan
+# dict2={
+#     'name' : 'vishnu',
+#     'age' : 25,
+#     'place' : 'Calicut'
+# }
+# print(dict2)
+# dict2['age']=20
+# print(dict2)
 
-# multiple values maattam, new itme add cheyam
-dict2.update({'email':'vishnu@gmail.com','age':24,'place':'Kozhikode'})
-print(dict2)
+# # multiple values maattam, new itme add cheyam
+# dict2.update({'email':'vishnu@gmail.com','age':24,'place':'Kozhikode'})
+# print(dict2)
 
-dict2.pop('email')
-print(dict2)
+# dict2.pop('email')
+# print(dict2)
 
-dict2.popitem()
-print(dict2)
+# dict2.popitem()
+# print(dict2)
+
+# unpacking
+
+x={'name' : 'achu','age':25,'place':'kozhikode'}
+
+for i in x:
+    print(x[i])
+print('-------------')
+    
+for i in x.values():
+    print(i)
+print('-------------')
+
+for i in x.keys():
+    print(i)
+print('-------------')
+
+for i in x.items():
+    print(i)
+print('-------------')
+
+for i,j in x.items():
+    print(i,j)
+print('-------------')
+
+y=x.copy() #ighane cheythal original dict chages varilla
